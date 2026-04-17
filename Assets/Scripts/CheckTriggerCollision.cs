adb80f8fd7e8283e78e5303f13e09d514527c7c7
Ander Belloso <ander.belloso@opendeusto.es>
2026-04-17 08:05:01 +0200

    CheckTriggerCollision script added

--- Assets/Scripts/CheckTriggerCollision.cs
+++ Assets/Scripts/CheckTriggerCollision.cs
@@ -34,7 +34,6 @@ public class CheckTriggerCollision : MonoBehaviour
         if (other.gameObject.name == "Capsule")
         {
             Debug.Log("He dejado de chocar con el trigger");
-            Destroy(this.gameObject);
         }
 
     }
